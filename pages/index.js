77c1154adc044d0aaa4e1e5e70f3cc3654e700d8
Akshit Kushwaha <akshit.kushwaha@wingify.com>
2023-05-04 14:14:36 +0530

    added styled components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import styled from 'styled-components';
 
 export default function Home() {
+
+  const StyledButton = styled.button`
+    background-color: #1E90FF;
+    color: white;
+    padding: 1%;
+    font-size: 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #007FFF;
+    }
+  `;
+
 
   return (
     <>
@@ -39,6 +55,7 @@ export default function Home() {
         </div>
 
         <h1>THIS IS A LARGE TEXT!</h1>
+        <StyledButton>Button1</StyledButton>
 
         <div className={styles.center}>
           <Image
